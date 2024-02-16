9b33abaffda8f90d000b33268f19880c780abea3
pasupathi07777 <pasupathi07777@gamil.com>
2024-02-16 21:16:10 +0530

    alt

--- index3.js
+++ index3.js
@@ -40,6 +40,9 @@ function loder() {
     })
     update_price()
 
+    
+    
+
 
 
 }
@@ -51,12 +54,12 @@ function display_slider() {
 }
 
 function slider_displaynone() {
-    slider.style.right = "-300px"
+    slider.style.right = "-250px"
 
 }
 function cart_delete() {
     console.log(cart_conatiner)
-    confirm("del")
+    // confirm("del")
 
     let name=this.parentElement.parentElement.querySelector(".product-name").innerHTML
     cart_conatiner=cart_conatiner.filter(a=>{
@@ -65,6 +68,15 @@ function cart_delete() {
     console.log(cart_conatiner)
 
     this.parentElement.parentElement.remove()
+    update_price()
+    total_cart_amount()
+
+    let cart_total=document.querySelector(".cart-box")
+    let pri=cart_total.textContent
+    pri=parseFloat(pri)
+    console.log(pri)
+    pri--
+    cart_total.textContent=pri
 
 
 }
@@ -75,7 +87,10 @@ function product_quantitys() {
         this.value = 1
     }
     let value=this.value
+    
     console.log(value)
+    update_price(this.value)
+    total_cart_amount()
 }
 
 function add_item() {
@@ -83,14 +98,14 @@ function add_item() {
     let name = cart_item.querySelector(".det-box h6").innerHTML
     let price = cart_item.querySelector(".det-box .price").innerHTML
     let image = cart_item.querySelector("img").src
-    // console.log(name, price, image)
+  
     if(cart_conatiner.find(a=> a==name)){
-        // console.log()
+       
         alert("is's already added")
         return
        
     }else{
-        let html=` <div class="box-inbox row row-cols-3 p-1 m-0 ">
+        let html=` <div class="box-inbox row row-cols-3 p-0 m-0 ">
     <div class="img-inbox p-0 ">
         <img src="${image}" alt="">
 
@@ -122,7 +137,16 @@ function add_item() {
     let inbox_cart = document.querySelector(".inbox")
     inbox_cart.append(div)
     cart_conatiner.push(name)
-    // console.log(cart_conatiner)
+    let cart_total=document.querySelector(".cart-box")
+    let pri=cart_total.textContent
+    pri=parseFloat(pri)
+    console.log(pri)
+    pri++
+    cart_total.textContent=pri
+    
+    
+
+  
     
     
    
@@ -137,11 +161,89 @@ function add_item() {
    
 
 }
-function update_price(){
+function update_price(value){
+    let tatal_amount=0
     let cart_box=document.querySelectorAll(".box-inbox")
-    console.log(cart_box)
-    let price=document.querySelector(".product-price").textContent
-    console.log(price)
+    cart_box.forEach((a)=>{
+        let price=a.querySelector(".product-price").innerHTML.replace("RS.","")
+        let qua=a.querySelector(".quantity").value
+        let total_inbox=a.querySelector(".total-inbox .total")
+        tatal_amount=Number(price)*qua
+        total_inbox.textContent=`RS.${tatal_amount}`
+       
+    })
+  
+
+    total_cart_amount()
+   
+    
+ 
+   
+   
 }
 
+
+function total_cart_amount(){
+    let total=0
+    let cart_box=document.querySelectorAll(".box-inbox")
+    let total_price=document.querySelector(".total-cart-amount .total-price")
+    cart_box.forEach((a)=>{
+        
+        let total_inbox=a.querySelector(".total-inbox .total").textContent.replace("RS.","")
+        total_inbox=Number(total_inbox)
+        console.log(total_inbox)
+
+        total+=total_inbox
+        
+      
+       
+       
+       
+    })
+    total_price.textContent=`RS.${total}`
+
+}
+
+
+
+let product_name = document.querySelectorAll(".det-box h6")
+let cart_btn = document.querySelector(".cart i")
+let x_btn = document.querySelector(".cancel-btn i")
+let slider_inbox = document.querySelector(".sliding-cart")
+let search_box = document.querySelector(".search-box input")
+
+
+search_box.addEventListener("keyup", () => {
+    let user_search = search_box.value.toLocaleLowerCase()
+ 
+   
+    product_name.forEach((a, i) => {
+
+
+        let product_name_text = a.textContent.toLocaleLowerCase()
+        console.log(product_name_text)
+        console.log(user_search)
+        console.log(product_name_text.indexOf(user_search))
+        if (product_name_text.indexOf(user_search) != 0) {
+            let f_par = a.parentElement
+            let s_par = f_par.parentElement
+            let t_par = s_par.parentElement
+            let final_parent = t_par.parentElement
+            console.log(final_parent)
+            final_parent.style.display = "none"
+5
+        } else {
+
+            let f_par = a.parentElement
+            let s_par = f_par.parentElement
+            let t_par = s_par.parentElement
+            let final_parent = t_par.parentElement
+            console.log(final_parent)
+            final_parent.style.display = "block"
+        }
+
+    })
+
+
+})
 
